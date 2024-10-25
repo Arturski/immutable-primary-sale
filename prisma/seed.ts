1b3ed7bd654a59037d99f1bb4a70490c4bcaabdb
Arthur <Arturski@users.noreply.github.com>
2024-10-25 19:31:08 +0200

    updated backend

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -20,44 +20,44 @@ async function main() {
             id: 'vi7age4ku18qynwbk4wx90ge',
             name: 'Shark',
             description: 'Evo Shark NFTs',
-            image: 'https://zacharycouchman.github.io/bb-example-metadata/tokens/Wil_BBH_HeroIcon_1.webp',
-            stockQuantity: 5000,
+            image: 'https://raw.githubusercontent.com/Arturski/public-static/main/immutable-zkevm-primary-sales/images/nfts/2.webp',
+            stockQuantity: 10000,
             status: 'active',
-            collectionAddress: '0x979013fa9be5acc6d31bf0c067c9677e9ea12864',
+            collectionAddress: '0x4ba8a1fac42eeac306c987524758786ce3d51931',
             contractType: 'ERC721',
             productPrices: {
                 create: [
                     {
                         currency_name: ethCurrency.name,
-                        amount: 1,
+                        amount: 0.01,
                     },
                 ],
             },
         },
     });
 
-    await prisma.product.upsert({
-        where: { id: 'jtwrclpj0v1zab865ne893hb' },
-        update: {},
-        create: {
-            id: 'jtwrclpj0v1zab865ne893hb',
-            name: 'Another Product',
-            description: 'Another NFT skin',
-            image: 'https://example.com/another-image.webp',
-            stockQuantity: 50,
-            status: 'active',
-            collectionAddress: '0x979013fa9be5acc6d31bf0c067c9677e9ea12864',
-            contractType: 'ERC721',
-            productPrices: {
-                create: [
-                    {
-                        currency_name: ethCurrency.name,
-                        amount: 1,
-                    },
-                ],
-            },
-        },
-    });
+    // await prisma.product.upsert({
+    //     where: { id: 'jtwrclpj0v1zab865ne893hb' },
+    //     update: {},
+    //     create: {
+    //         id: 'jtwrclpj0v1zab865ne893hb',
+    //         name: 'Another Product',
+    //         description: 'Another NFT skin',
+    //         image: 'https://example.com/another-image.webp',
+    //         stockQuantity: 50,
+    //         status: 'active',
+    //         collectionAddress: '0x4ba8a1fac42eeac306c987524758786ce3d51931',
+    //         contractType: 'ERC721',
+    //         productPrices: {
+    //             create: [
+    //                 {
+    //                     currency_name: ethCurrency.name,
+    //                     amount: 1,
+    //                 },
+    //             ],
+    //         },
+    //     },
+    // });
 }
 
 main()
